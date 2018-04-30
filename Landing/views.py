b1e90849466f9ddd2b9ba25a178d5993b408de06
tngusqkr1 <tngusqkr1@gmail.com>
2018-04-30 19:05:14 +0900

    bootstrap adopted

--- Landing/views.py
+++ Landing/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from .models import *
+
 
 # Create your views here.
 def exp_list(request):
-    return render(request, "Landing/exp_list.html", {})
+    exp_list = Exp.objects.all()
+
+    sc_list = School.objects.all()
+
+    return render(request, "Landing/exp_list.html",
+                  {'exp_list':exp_list,
+                   'sc_list': sc_list,})
+
+
